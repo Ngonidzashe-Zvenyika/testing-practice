e34311f53335accc11173a4ed74ebf2b26672086
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-10-30 21:41:58 +0200

    Add tests for analyze array function

---
+++ tests/analyze-array.test.js
@@ -0,0 +1,15 @@
+test('Given [1,8,3,4,2,6] determine that 4 is the average', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe(4);
+});
+
+test('Given [1,8,3,4,2,6] determine that 1 is the min', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe(1);
+});
+
+test('Given [1,8,3,4,2,6] determine that 8 is the max', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe(8);
+});
+
+test('Given [1,8,3,4,2,6] determine that 6 is the length', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe(6);
+});
